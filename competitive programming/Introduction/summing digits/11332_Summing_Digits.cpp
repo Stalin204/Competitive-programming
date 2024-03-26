ce61b99b7852f579642b83b9ba2584668ae0161c
Platon3 <118079978+Platon3@users.noreply.github.com>
2024-03-26 17:19:47 -0500

    Update 11332_Summing_Digits.cpp

--- competitive programming/Introduction/summing digits/11332_Summing_Digits.cpp
+++ competitive programming/Introduction/summing digits/11332_Summing_Digits.cpp
@@ -1,1 +1,46 @@
 
+#include <iostream>
+
+int digits(int numero) {
+    int digitos = 0;
+    while (numero != 0) {
+        int x = numero % 10;
+        numero /= 10;
+        digitos += 1;
+    }
+    return digitos;
+}
+
+int calculatorSum(int numero) {
+    int sum = 0;
+    while (numero != 0) {
+        int x = numero % 10;
+        sum += x;
+        numero /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int numero;
+    while (true) {
+        std::cin >> numero;
+        if (numero != 0) {
+            while (true) {
+                if (numero == 0) {
+                    std::cout << numero;
+                    break;
+                }
+                numero = calculatorSum(numero);
+
+                if (digits(numero) == 1) {
+                    std::cout << numero << std::endl;
+                    break;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
