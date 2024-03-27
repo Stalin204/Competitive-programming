0e1c8d8c019a2b6e8a9acb4028fc3cdeb129f34b
Platon3 <118079978+Platon3@users.noreply.github.com>
2024-03-27 14:02:27 -0500

    Delete competitive programming/Introduction/Automatic-Answer/AutomaticAnswer.py

--- competitive programming/Introduction/Automatic-Answer/AutomaticAnswer.py
+++
@@ -1,8 +0,0 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    if -1000 <= n <= 1000:
-        n = (((n * 567) // 9 + 7492) * 235 // 47 - 498)
-        centinela = str(n)
-        auxiliar = centinela[::-1]
-        print(auxiliar[1])
